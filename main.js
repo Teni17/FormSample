a383e5393d2f4a8e678059d6841850d44cff817c
Teni17 <tenibruyne@gmail.com>
2022-04-18 18:05:32 -0600

    hw2finished

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById("newuser").disabled= true;
+    document.getElementById("existinguser").disabled= true;
 
     document.getElementById("button1").addEventListener("click", function () {
 
-        document.getElementById("textbox1").value = "success";
+        document.getElementById("welcome").disabled= true;
+        document.getElementById("newuser").disabled= false;
+        document.getElementById("existinguser").disabled= true;
+    });
+    document.getElementById("button2").addEventListener("click", function (){
+        document.getElementById("welcome").disabled= true;
+        document.getElementById("newuser").disabled= true;
+        document.getElementById("existinguser").disabled= false;
     });
 });
-
+function newAlert(){
+    let name = document.getElementById("username").value;
+    alert("welcome " + name);
+}
+function existAlert(){
+    let name = document.getElementById("existusername").value;
+    alert("welcome back " + name);
+}
